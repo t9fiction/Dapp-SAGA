a066c899147aa59db1ca0cc805ac67bf64bc203c
SagitarriusA <sohail.sohailishaq@gmail.com>
2022-01-02 19:32:43 +0500

    all

---
+++ smart_contract/scripts/deploy.js
@@ -0,0 +1,26 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+const hre = require("hardhat");
+
+const main = async () => {
+  
+  // We get the contract to deploy
+  const Transactions = await hre.ethers.getContractFactory("Transactions");
+  const transactions = await Transactions.deploy("Hello, Hardhat!");
+
+  await transactions.deployed();
+
+  console.log("Contract-Transactions deployed to:", transactions.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
